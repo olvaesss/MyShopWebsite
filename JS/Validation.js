588f012013bd4fb73b2d69da0ef329d773de303b
olvaesss <100799717+olvaesss@users.noreply.github.com>
2023-03-10 16:04:08 +0300

    Update 1.2
    Update 1.2

--- JS/Validation.js
+++ JS/Validation.js
@@ -1,12 +1,27 @@
 function PasswordValid(input){
-    input=document.querySelector(".Password_Input").value
     let arr_banned=['!','@','#','$','%','^','&','*','(',')','-','_','=','+',' ',',','<','.','>','/','?',']','[']
-    input=input.split()
-    for(let i=0;i<=input.length;i++){
-        for(let j=0;j<=arr_banned.length;j++){
-            if(input[i]===arr_banned[j]){
-                alert("Error 1")
-            }
+    let flag=true
+    for(let i=0;i<=arr_banned.length;i++){
+        if(input.includes(arr_banned[i])===true){
+            flag=false
+            break
         }
+    }
+    if(flag===true){
+    alert("Write")
+    document.querySelector(".Wrong_Symbols_password").setAttribute(hidden,'hidden')}
+    else{
+    alert("Wrong")
+    document.querySelector(".Wrong_Symbols_password").removeAttribute(hidden='hidden')}
+}
+
+function DrawPassword(Input, Input_Repeat){
+    Input=document.querySelector('.Password_Input').value
+    Input_Repeat=document.querySelector('.Password_Input_Repeat').value
+    if(Input==Input_Repeat){
+        PasswordValid(Input)
+        document.querySelector(".Passwords_Dont_Mathces").setAttribute(hidden,'hidden')
+    }else{
+        document.querySelector(".Passwords_Dont_Mathces").removeAttribute(hidden='hidden')
     }
 }
--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const app = require('express')()
 const http = require('http')
-const express = require('express')
 const host = '127.0.0.1'
 const port = 7000
+const fs = require('fs')
+const server = http.createServer((req, res)=>{
 
-
-
-app.listen(port, host, function () {
-    console.log(`Server listens http://${host}:${port}`)
+    if(req.url === 'index.html')
+    {
+        fs.readFile('./HTML/index.html',()=>{
+            res.writeHead(200, {"Content-Type": "text/html"})
+            res.end()
+        })
+    }
 })
+
+server.listen( port, host ,()=>
+{
+    console.log("server runs: http://"+host+':'+port)
+})
+
