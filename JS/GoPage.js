c41238f5226f2cb69e85d74fccf0d1ddeb9766df
olvaesss <100799717+olvaesss@users.noreply.github.com>
2023-03-16 17:26:49 +0300

    Update 1.4
    Change style, update function of opening page, update fonts

---
+++ JS/GoPage.js
@@ -0,0 +1,9 @@
+function GoPage(value){
+    window.location.href = value
+}
+
+function KeepGoing(){
+    if(value=="Register.html"||value=="Login.html"){
+        window.location.href = "index.html"
+    }    
+}
