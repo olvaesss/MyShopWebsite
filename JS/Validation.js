e503e75691976de3fd6e0a2d53546095b3f3b8ff
olvaesss <100799717+olvaesss@users.noreply.github.com>
2023-03-21 19:13:29 +0300

    Update 1.6
    Finaly end Validations functions on register. start adding regulars for e-mail, adding hidden div for user space on index page

--- JS/Validation.js
+++ JS/Validation.js
@@ -1,27 +1,47 @@
-function PasswordValid(input){
+ let GoFlag=true
+function DrawPassword(Input, Input_Repeat){
+    Input=document.querySelector('.Password_Input').value
+    Input_Repeat=document.querySelector('.Password_Input_Repeat').value
+    if(Input==Input_Repeat){
+        document.querySelector(".Passwords_Dont_Mathces").hidden='hidden'
+    }else{
+        document.querySelector(".Passwords_Dont_Mathces").removeAttribute(hidden='hidden')
+        GoFlag=false
+    }
     let arr_banned=['!','@','#','$','%','^','&','*','(',')','-','_','=','+',' ',',','<','.','>','/','?',']','[']
     let flag=true
     for(let i=0;i<=arr_banned.length;i++){
-        if(input.includes(arr_banned[i])===true){
+        if(Input.includes(arr_banned[i])===true){
             flag=false
             break
         }
     }
     if(flag===true){
-    alert("Write")
-    document.querySelector(".Wrong_Symbols_password").setAttribute(hidden,'hidden')}
+        document.querySelector(".Wrong_Symbols_password").hidden='hidden'}
     else{
-    alert("Wrong")
-    document.querySelector(".Wrong_Symbols_password").removeAttribute(hidden='hidden')}
+        document.querySelector(".Wrong_Symbols_password").removeAttribute(hidden='hidden')
+        GoFlag=false
+    }
+    console.log(Input.length)
+    if(Input.length >= 8){
+        document.querySelector(".Minimal_Password_Range").hidden='hidden'
+    }
+    else{
+        document.querySelector(".Minimal_Password_Range").removeAttribute(hidden='hidden')
+        GoFlag=false
+    }
+    Go()
 }
 
-function DrawPassword(Input, Input_Repeat){
-    Input=document.querySelector('.Password_Input').value
-    Input_Repeat=document.querySelector('.Password_Input_Repeat').value
-    if(Input==Input_Repeat){
-        document.querySelector(".Passwords_Dont_Mathces").setAttribute(hidden,'hidden')
-    }else{
-        document.querySelector(".Passwords_Dont_Mathces").removeAttribute(hidden='hidden')
-    }
-    PasswordValid(Input)
-}
+// function Go(){
+//     if(GoFlag===true)
+//     {
+//         window.location.href = "index.html"
+//         document.querySelector(".Buttons").hidden='hidden'
+//         document.querySelector(".UserArea").removeAttribute(hidden='hidden')
+//         alert("Успешная регистрация!")
+//     }
+//     else{
+//         alert("Server Error")
+//     }
+// }
