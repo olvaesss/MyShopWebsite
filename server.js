d80b7a1c93342029449e58cd9cca73e3ddc372a7
olvaesss <100799717+olvaesss@users.noreply.github.com>
2023-04-13 13:35:40 +0300

    Update 2.3

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express')
+const db= require('@firebase/firestore');
+const express = require('express');
 const server = express();
-const path = require ('path')
+const path = require ('path');
 const urlencodedParser = express.urlencoded({extended: false});
 const firebaseConfig = {
   apiKey: "AIzaSyBg_gX6hmBiNxzbALLm7XsI7gjHtkYVDhQ",
@@ -12,18 +13,37 @@ const firebaseConfig = {
   measurementId: "G-1M8BFDRTBZ"
 };
 
+let count=0
+
+class User{
+    constructor(ID){
+        this.ID=ID
+    }
+    Name=String;
+    Email=String;
+    Password=String;
+}
+
+
 
 server.use(express.static(path.join(__dirname, 'public')));
 
 server.post('/Register', urlencodedParser, (req,res)=>{
-    console.log(req.body);
-    res.send(`${req.body.Name} - ${req.body.Email}- ${req.body.Password}`);
+    count+=1;
+    let newUser = new User(count);
+    newUser.Name=req.body.Name;
+    newUser.Email=req.body.Email;
+    newUser.Password=req.body.Password;
+    console.log(newUser);
     res.sendFile(`${__dirname}/public/HTML/index.html`);
 });
 
 server.post('/Login', urlencodedParser, (req,res)=>{
     console.log(req.body);
-    res.send(`${req.body.Login}- ${req.body.Password}`);
+    let LogUser = new User;
+    // Полуучение Почты и Пароля с бд
+    // LogUser.Name=
+    // LogUser.Password=
     res.sendFile(`${__dirname}/public/HTML/index.html`);
 });
 
@@ -55,3 +75,47 @@ server.get('/Check_Register',(req,res)=>{
 server.listen(2000,()=>{
     console.log("Server running at http://localhost:2000/")
 })
+
+
+
+
+
+let UserFlag=false
+function DrawPassword(Input, Input_Repeat){
+    Input=document.querySelector('.Password_Input').value
+    Input_Repeat=document.querySelector('.Password_Input_Repeat').value
+    if(Input==Input_Repeat){
+        document.querySelector(".Passwords_Dont_Mathces").hidden='hidden'
+        UserFlag=true
+    }else{  
+        document.querySelector(".Passwords_Dont_Mathces").removeAttribute(hidden='hidden')
+        UserFlag=false
+    }
+    let arr_banned=['!','@','#','$','%','^','&','*','(',')','-','_','=','+',' ',',','<','.','>','/','?',']','[']
+    let flag=true
+    for(let i=0;i<=arr_banned.length;i++){
+        if(Input.includes(arr_banned[i])===true){
+            flag=false
+            break
+        }
+    }
+    if(flag===true){
+        UserFlag=true
+        document.querySelector(".Wrong_Symbols_password").hidden='hidden'}
+    else{
+        document.querySelector(".Wrong_Symbols_password").removeAttribute(hidden='hidden')
+        UserFlag=false
+    }
+    console.log(Input.length)
+    if(Input.length >= 8){
+        document.querySelector(".Minimal_Password_Range").hidden='hidden'
+        UserFlag=true
+    }
+    else{
+        document.querySelector(".Minimal_Password_Range").removeAttribute(hidden='hidden')
+        UserFlag=false
+    }
+    if(UserFlag===true){
+        
+    }
+}
